8ea5280f31184485119e8820d65533d8d6e123b9
spiritualsection2703 <88161451+spiritualsection2703@users.noreply.github.com>
2023-11-18 23:20:28 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,46 +4,54 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
-
-
+import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-           if (args.length != 2) {
-            System.out.println("Usage: java FileConverter <source.xxx> <destination.yyy>");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter conversion direction (csv to txt or txt to csv, or 'quit' to exit): ");
+        String conversionDirection = scanner.nextLine().toLowerCase();
+
+        if (conversionDirection.equals("quit")) {
+            System.out.println("Thank you for using our program!");
             return;
         }
-         
-        String sourceFileName = args[0];
-        String destinationFileName = args[1];
+
+        System.out.print("Enter source file name: ");
+        String sourceFileName = scanner.nextLine();
+
+        System.out.print("Enter destination file name: ");
+        String destinationFileName = scanner.nextLine();
 
         try {
-            //This determines the source and destination of the file formats
-            String sourceFormat = getFileFormat(sourceFileName);
-            String destinationFormat = getFileFormat(destinationFileName);
 
-            //Cheks if its csv or txt
+            // This determines the source and destination of the file formats
+            String sourceFormat = getFileFormat(sourceFileName);
+            String destinationFormat = getDestinationFormat(sourceFormat, conversionDirection);
+
+            // Checks if it's csv or txt
             if (!isValidFormat(sourceFormat) || !isValidFormat(destinationFormat)) {
                 System.out.println("Please use formats csv or txt.");
                 return;
             }
 
-            //Reads content from the files
+            //This reads content from the source file
             String fileContent = readFileContent(sourceFileName);
 
-            //Write the content to the file
+            //This writes the content to the destination
             writeFileContent(destinationFileName, fileContent);
 
             System.out.println("File conversion completed.");
         } catch (IOException e) {
             System.out.println("An error occurred: " + e.getMessage());
+        } finally {
+            scanner.close();
         }
     }
-    
-    
+
+
     private static String getFileFormat(String fileName) {
         int dotIndex = fileName.lastIndexOf('.');
         if (dotIndex != -1) {
@@ -61,6 +69,16 @@ public class Main {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
+                String[] cells = line.split(",");
+                for (int i = 0; i < cells.length; i++) {
+                    //This checks if the cell has a string
+                    if (cells[i].length() > 13) {
+                        // Normalize the string value
+                        cells[i] = cells[i].substring(0, 10) + "...";
+                    }
+                }
+                //This replaces empty cells with "N/A"
+                line = String.join(",", cells).replaceAll(",(?=,)|,$", ",N/A");
                 content.append(line).append("\n");
             }
         }
@@ -72,5 +90,16 @@ public class Main {
             writer.write(content);
         }
     }
-}
 
+    private static String getDestinationFormat(String sourceFormat, String conversionDirection) {
+        if (conversionDirection.equals("csv to txt")) {
+            return "txt";
+        } else if (conversionDirection.equals("txt to csv")) {
+            return "csv";
+        } else {
+            System.out.println("Invalid conversion direction. Please use 'csv to txt' or 'txt to csv'.");
+            System.exit(1);
+            return "";
+        }
+    }
+}
